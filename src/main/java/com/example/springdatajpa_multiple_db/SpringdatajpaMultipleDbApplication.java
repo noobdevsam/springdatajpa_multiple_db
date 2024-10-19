f7dac75e20cf4828e37aab73c689b061e9b93f5f
mrdevsam <mdshoykotislam05@gmail.com>
2024-10-19 20:04:13 +0600

    initial commit

---
+++ src/main/java/com/example/springdatajpa_multiple_db/SpringdatajpaMultipleDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdatajpa_multiple_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdatajpaMultipleDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdatajpaMultipleDbApplication.class, args);
+	}
+
+}
