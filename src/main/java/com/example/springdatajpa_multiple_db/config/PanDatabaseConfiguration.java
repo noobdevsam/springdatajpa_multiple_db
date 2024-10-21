7656d0d9e697789740444bc8dd71ed408cca177c
mrdevsam <mdshoykotislam05@gmail.com>
2024-10-21 06:44:08 +0100

    connecting data sources

--- src/main/java/com/example/springdatajpa_multiple_db/config/PanDatabaseConfiguration.java
+++ src/main/java/com/example/springdatajpa_multiple_db/config/PanDatabaseConfiguration.java
@@ -1,10 +1,14 @@
 package com.example.springdatajpa_multiple_db.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class PanDatabaseConfiguration {
@@ -14,5 +18,11 @@ public class PanDatabaseConfiguration {
     @ConfigurationProperties("spring.pan.datasource")
     public DataSourceProperties panDataSourceProperties() {
         return new DataSourceProperties();
+    }
+
+    @Bean
+    public DataSource panHolderDataSource(@Qualifier(value = "panDataSourceProperties") DataSourceProperties panDataSourceProperties) {
+        return panDataSourceProperties.initializeDataSourceBuilder()
+                .type(HikariDataSource.class).build();
     }
 }
